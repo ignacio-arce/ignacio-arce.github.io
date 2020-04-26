9815ad718544c08398fc8aae0dc49b922104971b
Ignacio Arce <iarce@dc.uba.ar>
2020-04-26 02:19:22 -0300

    Covid 19 estilo cambiado

--- assets/js/covid.js
+++ assets/js/covid.js
@@ -2,14 +2,16 @@
 const covidTrack = () => {
 
 	function reqListener() {
-        console.log(this.responseText);
-		document.getElementById('covid').innerHTML += JSON.parse(this.responseText)[6].Confirmed;
+		const covidBadge = document.getElementById('covid');
+		document.getElementById('covid-confirmed').innerHTML += JSON.parse(this.responseText)[6].Confirmed;
+		covidBadge.classList.add('show');
+		covidBadge.classList.remove('hide');
     }
 
     let oReq = new XMLHttpRequest();
     let country = 'argentina';
 	oReq.addEventListener('load', reqListener);
-	oReq.open('GET', `https://api.covid19api.com/live/country/${country}/status/confirmed`);
+	oReq.open('GET', `https://api.covid19api.com/live/country/${country}`);
     oReq.send();
 };
 
